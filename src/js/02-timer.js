c23d8ad7d0db99dc1e31a39644312b2ef17ec194
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-23 01:29:36 +0200

    alfaTry

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,16 @@
 import flatpickr from "flatpickr";
 // Dodatkowy import stylów
 import "flatpickr/dist/flatpickr.min.css";
+
+
+const alfa = 'mango'
+
+const btnx = document.querySelector('button')
+
+const logShow = () => {
+    console.log(alfa)
+}
+
+btnx.addEventListener('click', logShow )
+
+// console.log('mango')
