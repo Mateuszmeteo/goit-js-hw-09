49f4af3db6cd42646bf46b27cdca82ef2fcd734e
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-22 04:02:33 +0200

    first ex

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,66 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const bgdColor = document.querySelector('body')
+
+let timerSet = null;
+
+btnStart.addEventListener('click', () => {
+    timerSet = setInterval(() => {
+        console.log(getRandomHexColor())
+        bgdColor.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerSet);
+});
+
+
+
+// console.log(btnStart)
+// console.log(btnStop)
+// console.log(bgdColor)
+
+
+
+
+
+// const colorStart = () => {
+//     setTimeout(() => {
+//        bgdColor.style.backgroundColor = getRandomHexColor()
+//     },1000);
+// }
+
+// btnStart.addEventListener('click', colorStart)
+
+
+
+
+
+// const colorStart = () => {
+//     setTimeout(() => {
+//        document.body.style.backgroundColor = getRandomHexColor()
+//     },1000);
+// }
+
+
+
+
+// 
+// btnStart.addEventListener('click' () = {
+//     setTimeout(() => {
+//         bgdColor.style.backgroundColor = getRandomHexColor()
+//     }, 1000);
+    
+
+// })
+
+
+
+
