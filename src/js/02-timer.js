b8c11b36d96a306d16624b840c2439c9426ca5be
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-24 01:44:27 +0200

    ex2-first-try

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,40 @@ import "flatpickr/dist/flatpickr.min.css";
 
 console.log('manog')
 
+flatpickr('#datetime-picker')
+const inputSelect = document.querySelector('#datetime-picker')
+
+
+
+
+// const actualDate = new Date()
+
+// console.log(actualDate.)
+
+flatpickr (selector, options) 
+    const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+  if (defaultDate > inputSelect.value) {
+    return alert('x')
+  }
+
+
+
+
+///////////////////////////////////////////////..........//////////////////////////////////////
+
+
+
+
+
 // const alfa = 'mango'
 
 // const btnStart = document.querySelector('button')
